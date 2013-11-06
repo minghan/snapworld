bb106c2813723a6e190000d3158f8acc065e8b87
Nikhil Khadke <nikhil.khadke@gmail.com>
2013-11-05 16:24:27 -0800

    modified test

--- Test.py
+++ Test.py
@@ -62,25 +62,26 @@ def GenConfigFile(var_node, var_range, var_stat_tasks, var_gen_tasks, var_drange
     config = open(sys.argv[1], 'w')
     try:
         for line in dat:
-            line = line.strip(" ").strip("\t")
-            if (len(line) > 0):
-                if (line[0] != '#' and line.find("var") >= 0 and line.find("node") >= 0):
-                    line = line.replace(''.join(x for x in line if x.isdigit()), str(var_node))
-                if (line[0] != '#' and line.find("var") >= 0 and line.find("range") >= 0):
-                    line = line.replace(''.join(x for x in line if x.isdigit()), str(var_range))
-                if (line[0] != '#' and line.find("var") >= 0 and line.find("stat_tasks") >= 0):
-                    line = line.replace(''.join(x for x in line if x.isdigit()), str(var_stat_tasks))
-                if (line[0] != '#' and line.find("var") >= 0 and line.find("gen_tasks") >= 0):
-                    line = line.replace(''.join(x for x in line if x.isdigit()), str(var_gen_tasks))
-                if (line[0] != '#' and line.find("var") >= 0 and line.find("drange") >= 0):
-                    line = line.replace(''.join(x for x in line if x.isdigit()), str(var_drange))
+            if (len(line) > 0 and len(line.split()) > 1 and line.split()[0] == 'var'):
+                line_arr = line.split()
+                if (line_arr[0] == 'var' and line_arr[1] == 'nodes'):
+                    line = "var    nodes     %s\n" % str(var_node)
+                if (line_arr[0] == 'var' and line_arr[1] == 'range'):
+                    line = "var    range     %s\n" % str(var_range)
+                if (line_arr[0] == 'var' and line_arr[1] == 'stat_tasks'):
+                    line = "var    stat_tasks     %s\n" % str(var_stat_tasks)
+                if (line_arr[0] == 'var' and line_arr[1] == 'gen_tasks'):
+                    line = "var    gen_tasks     %s\n" % str(var_gen_tasks)
+                if (line_arr[0] == 'var' and line_arr[1] == 'drange'):
+                    line = "var    drange     %s\n" % str(var_drange)
             config.write(line)
     finally:
         config.close()
 
 def Run(var_node, var_range, var_stat_tasks, var_gen_tasks, var_drange):
-    #    os.system("rake cleanup; rake test")
-    os.system("grep \"\[master\]\" /afs/cs.stanford.edu/u/senwu/lfs/master.log > tmp.txt")
+    os.system("sleep 5")
+    os.system("rake cleanup; rake test")
+    os.system("grep \"\[master\]\" /afs/cs.stanford.edu/u/$USER/lfs/$USER/master.log > tmp.txt")
     input = open('tmp.txt', 'r')
     st = ""
     for dat in input:
@@ -88,7 +89,8 @@ def Run(var_node, var_range, var_stat_tasks, var_gen_tasks, var_drange):
     st = st.split(" ")
     output = open("test.log", "a")
     try:
-        output.write("%d\t%d\t%d\t%d\t%d\t%s" % (var_node, var_range, var_stat_tasks, var_gen_tasks, var_drange, st[len(st) - 2]))
+        data = "%d\t%d\t%d\t%d\t%d\t%s" % (var_node, var_range, var_stat_tasks, var_gen_tasks, var_drange, st[len(st) - 2])
+        output.write(data + "\n")
         output.flush()
     finally:
         output.close()
@@ -98,20 +100,18 @@ if __name__ == '__main__':
     var_node = int(sys.argv[2])
     prime = GenPrime(var_node)
     GenCandidate(var_node)
-    candidate = sorted(candidate)
+    candidate = filter(lambda x: x%5000 == 0, sorted(candidate))
     print candidate
     
     for var_range in candidate:
-        for var_stat_tasks in candidate:
+        for var_drange in candidate:
             var_gen_tasks = var_node / var_range
-            var_drange = var_node / var_stat_tasks
-            if (var_range >= 100 and var_drange >= 100):
+            var_stat_tasks = var_node / var_drange
+            if (var_range >= 20000 and var_drange >= 20000):
                 if (num == 1):
                     print var_node, var_range, var_stat_tasks, var_gen_tasks, var_drange
+                print >> sys.stderr,  var_node, var_range, var_stat_tasks, var_gen_tasks, var_drange
                 GenConfigFile(var_node, var_range, var_stat_tasks, var_gen_tasks, var_drange)
-                num -= 1
-            if num == 0:
-                exit(0)
-    #            Run(var_node, var_range, var_stat_tasks, var_gen_tasks, var_drange)
+                Run(var_node, var_range, var_stat_tasks, var_gen_tasks, var_drange)
     print "Test done!"
 
